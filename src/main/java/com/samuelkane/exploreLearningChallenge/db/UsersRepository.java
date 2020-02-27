917731260ac8dd0cd32fd7d1a0c4fc852122cfd7
Jess Niblo <jessniblo@Jesss-iMac.local>
2020-02-27 15:12:15 -0500

    feat: add user dto and repository
    Pretty basic files. Both are near final just working out the kinks with H2. The reposity is going to need another method to check if a user already exists with a given first name/last name combo

---
+++ src/main/java/com/samuelkane/exploreLearningChallenge/db/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.samuelkane.exploreLearningChallenge.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<User, Long> {
+
+}
