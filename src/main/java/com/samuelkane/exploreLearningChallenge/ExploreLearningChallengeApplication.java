224c9d7a0e60f12c592e08a814d1db7474d4721d
Jess Niblo <jessniblo@Jesss-iMac.local>
2020-02-27 12:52:08 -0500

    feat: intial configuration

---
+++ src/main/java/com/samuelkane/exploreLearningChallenge/ExploreLearningChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.samuelkane.exploreLearningChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreLearningChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreLearningChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelkane/exploreLearningChallenge/ExploreLearningChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelkane.exploreLearningChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreLearningChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
