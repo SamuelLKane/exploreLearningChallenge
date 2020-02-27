54cbc60a38b64a2e0b937d8102071a2cf64be645
Jess Niblo <jessniblo@Jesss-iMac.local>
2020-02-27 15:10:11 -0500

    feat: updating base application file
    This is related to getting h2 working. Still struggling through this.
    Stopping here for now in favor of getting the rest of the skeleton wireframed
    out and working

--- src/main/java/com/samuelkane/exploreLearningChallenge/ExploreLearningChallengeApplication.java
+++ src/main/java/com/samuelkane/exploreLearningChallenge/ExploreLearningChallengeApplication.java
@@ -2,10 +2,33 @@ package com.samuelkane.exploreLearningChallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ExploreLearningChallengeApplication {
 
+	public WebSecurityConfigurerAdapter webSecurityConfig(DataSource dataSource) {
+		return new WebSecurityConfigurerAdapter() {
+			@Override
+			protected void configure(HttpSecurity http) throws Exception {
+				http.authorizeRequests()
+						.antMatchers("/").permitAll()
+						.and().authorizeRequests().antMatchers("/h2-console/**").permitAll();
+
+				http.csrf().disable();
+				http.headers().frameOptions().sameOrigin();
+			}
+
+			@Override
+			protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+				auth.jdbcAuthentication().dataSource(dataSource);
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ExploreLearningChallengeApplication.class, args);
 	}
