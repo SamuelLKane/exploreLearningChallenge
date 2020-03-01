4f00c3d35dbc0569f8cf3004db81beabe2ae9b5a
Jess Niblo <jessniblo@Jesss-iMac.local>
2020-03-01 16:55:28 -0500

    fix: drop extra import
    Just cleaning up

--- src/test/java/com/samuelkane/exploreLearningChallenge/UserControllerTest.java
+++ src/test/java/com/samuelkane/exploreLearningChallenge/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.samuelkane.exploreLearningChallenge;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
